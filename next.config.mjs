d324ec3cdd86b024f276ed3dacb8de4e556a85c7
Roopaish <rupesh39943@gmail.com>
2024-11-05 21:05:25 +0545

    upgrade packages: next, styled-components, antd

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
   compiler: {
     styledComponents: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,19 +1,38 @@
 "use client"
 
-import { type ReactNode } from "react"
+import { useEffect, type ReactNode } from "react"
+import { useGlobalNotification } from "@/store/global-notification"
 import { customTheme, styledComponentsTheme } from "@/theme"
 import GlobalStyles from "@/theme/global-styles"
-import { ConfigProvider } from "antd"
+import { ConfigProvider, notification } from "antd"
 import { ThemeProvider } from "styled-components"
 
 import StyledComponentsRegistry from "./styled-components-registry"
 
 export default function Providers({ children }: { children: ReactNode }) {
+  const [api, contextHolder] = notification.useNotification()
+  const { setNotificationFunctions } = useGlobalNotification()
+
+  useEffect(() => {
+    if (api) {
+      setNotificationFunctions({
+        success: api["success"],
+        error: api["error"],
+        info: api["info"],
+        warning: api["warning"],
+        destroy: api["destroy"],
+      })
+    }
+  }, [api])
+
   return (
     <StyledComponentsRegistry>
       <ThemeProvider theme={styledComponentsTheme}>
         <GlobalStyles />
-        <ConfigProvider theme={customTheme}>{children}</ConfigProvider>
+        <ConfigProvider theme={customTheme}>
+          {children}
+          {contextHolder}
+        </ConfigProvider>
       </ThemeProvider>
     </StyledComponentsRegistry>
   )
